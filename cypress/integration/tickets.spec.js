9f54c9b83a0703b1e236aaa3d9468c185f4ed53f
Jefferson Melo <jefferson.mello8@hotmail.com>
2020-08-15 08:22:15 -0300

    manipulei campos do tipo input inserindo informações

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,4 +1,18 @@
 describe("Tickets", () => {
     beforeEach(() => cy.visit("https://bit.ly/2XSuwCW"));
+
+    it.only("fills all the text input fields", () => {
+        const firstName = "Jefferson";
+        const lastName = "Melo";
+
+        cy.get("#first-name").type(firstName);
+        cy.get("#last-name").type(lastName);
+        cy.get("#email").type("jefferson.mello8@hotmail.com");
+        cy.get("#requests").type("Nada a declarar");
+        cy.get("#signature").type(`${firstName} ${lastName}`);        
+    });
+
     it("has 'TICKETBOX' header's heading", () => {});
+
+
 });
